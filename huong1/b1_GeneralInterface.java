2120dfd349cfa50904e9f3f40cb8e2d12fe88db3
HONG3824620 <deathmia1209@gmail.com>
2024-09-22 22:55:28 +0700

    Add files via upload

---
+++ huong1/b1_GeneralInterface.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package netbean_17_buoihocdautien;
+
+
+
+public class b1_GeneralInterface {
+    
+}
